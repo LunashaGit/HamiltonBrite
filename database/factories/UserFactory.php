8210673cdf18d570d476fc2be5b1ce31cfa425cd
LunashaGit <lunamuylkens@gmail.com>
2022-05-17 10:39:04 +0200

    Fix Profile TEXT

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use phpDocumentor\Reflection\Types\Boolean;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,7 +28,7 @@
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         @auth
             <div>
-                <a href="/profil">Profil page</a>
+                <a href="/profile">Profile page</a>
                 <form method="POST" action="/logout">
                     @csrf
                     <input type="submit" value="Log Out">
