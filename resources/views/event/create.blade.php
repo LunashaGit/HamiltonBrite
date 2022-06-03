a44d6c149d6731a3e7b8098ff9a0ff49f1671837
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2022-06-03 13:10:52 +0200

    Resolve crash after 2 methods

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -98,9 +98,7 @@
                         </div>
 
                     </form>
-                    <input type="text" id="mapinput">
-                    <input type="submit" id="submitmap">
-                    <p id="pipoupi"></p>
+
                     <style>
                         #map {
                             height: 15rem;
@@ -108,40 +106,7 @@
                         }
 
                     </style>
-                    <script>
-                        const mapUp = () => {
-                            let city = document.getElementById('mapinput').value
-                            var requestOptions = {
-                                method: 'GET',
-                            };
-                            console.log(city)
 
-                            fetch(`https://api.geoapify.com/v1/geocode/search?text=${city}&apiKey=a203d55a7a1f46cda1aef5ce6655c14c`, requestOptions)
-                                .then(response => response.json())
-                                .then(result => {
-                                    console.log(result.features[0].geometry.coordinates[0])
-                                    console.log(result.features[0].geometry.coordinates[1])
-                                    document.getElementById('latitude').value = result.features[0].geometry.coordinates[1]
-                                    document.getElementById('longitude').value = result.features[0].geometry.coordinates[0]
-
-                                    let map = L.map('map').setView([result.features[0].geometry.coordinates[1], result.features[0].geometry.coordinates[0]], 12);
-                                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                    }).addTo(map);
-
-                                    L.marker([result.features[0].geometry.coordinates[1], result.features[0].geometry.coordinates[0]]).addTo(map)
-                                        .bindPopup('The search :')
-                                        .openPopup();
-                                })
-                                .catch(error => {
-                                    console.log('error', error)
-                                    document.getElementById('pipoupi').innerText = "Street not found"
-                                });
-
-
-                        }
-                        document.getElementById('submitmap').addEventListener('click', mapUp)
-                    </script>
                     @if ($errors->any())
                         <ul>
                             @foreach ($errors->all() as $error)
@@ -151,7 +116,41 @@
                     @endif
                 </div>
         </section>
-        <div id="map"></div>
+        <input autocomplete="off" type="text" id="mapinput">
+        <input type="submit" id="submitmap">
+        <p id="pipoupi"></p>
+        <div id="here"></div>
+        <script>
+            const mapUp = () => {
+                let city = document.getElementById('mapinput').value
+                var requestOptions = {
+                    method: 'GET',
+                };
+                fetch(`https://api.geoapify.com/v1/geocode/search?text=${city}&apiKey=a203d55a7a1f46cda1aef5ce6655c14c`, requestOptions)
+                    .then(response => response.json())
+                    .then(result => {
 
+                        document.getElementById('latitude').value = result.features[0].geometry.coordinates[1]
+                        document.getElementById('longitude').value = result.features[0].geometry.coordinates[0]
+                        document.getElementById('here').innerHTML = `<div id="map"></div>`
+
+                        var map = L.map('map').setView([result.features[0].geometry.coordinates[1], result.features[0].geometry.coordinates[0]], 12);
+                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        }).addTo(map);
+
+                        L.marker([result.features[0].geometry.coordinates[1], result.features[0].geometry.coordinates[0]]).addTo(map)
+                            .bindPopup('The search :')
+                            .openPopup();
+                    })
+                    .catch(error => {
+                        console.log('error', error)
+                        document.getElementById('pipoupi').innerText = "Street not found"
+                    });
+
+
+            }
+            document.getElementById('submitmap').addEventListener('click', mapUp)
+        </script>
     </x-slot>
 </x-layout>
